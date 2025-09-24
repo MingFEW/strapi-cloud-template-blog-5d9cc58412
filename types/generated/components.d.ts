75309670d3a401b0d718be59c5ee33d793eb85e5
MingFEW <mingfework@gmail.com>
2025-09-24 20:48:15 +0700

    Update mapLink

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -154,7 +154,7 @@ export interface SharedContactBlk extends Struct.ComponentSchema {
     email: Schema.Attribute.String;
     img: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     mapImg: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
-    mapLink: Schema.Attribute.String;
+    mapLink: Schema.Attribute.Text;
     openTime: Schema.Attribute.Text;
     phone: Schema.Attribute.String;
   };
