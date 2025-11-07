50d32900922b59ffec3275cac9ac7a816fd10c89
MingFEW <mingfework@gmail.com>
2025-11-07 09:37:24 +0700

    Update code import

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = () => ({});
+module.exports = () => ({
+  "strapi-csv-import-export": {
+    config: {
+      authorizedExports: ["api::machine.machine"],
+      authorizedImports: ["api::machine.machine"]
+    }
+  }
+});
