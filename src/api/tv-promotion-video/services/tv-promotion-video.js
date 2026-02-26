80bfe4ea978c169623a0f1d5d8bef8e77f9af8b0
MingFEW <mingfework@gmail.com>
2026-02-26 15:28:55 +0700

    Update the database tv page, htmlContent

---
+++ src/api/tv-promotion-video/services/tv-promotion-video.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tv-promotion-video service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::tv-promotion-video.tv-promotion-video');
